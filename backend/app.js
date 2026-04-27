050b18102a501508d3fae25a7346370eb6f7e0b6
NeonAlex117 <jesusalejandrojs@hotmail.com>
2026-04-27 09:19:39 -0600

    fix

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,8 @@ const allowedOrigins = [
   'http://localhost:5173',
 ].filter(Boolean);
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error('Not allowed by CORS'));
-  }
-}));
+app.use(cors());
+app.use(express.json());
 
 app.use(express.json());
 
